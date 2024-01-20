a89d8dd9e0ea126b004fd12aadaff70593e7ac3a
ptrstr <ptrstr@protonmail.com>
2024-01-20 11:38:06 -0500

    Add daisyUI

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,16 +1,15 @@
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
  kit: {
   adapter: adapter()
  },
-
+ preprocess: vitePreprocess(),
  vitePlugin: {
-  experimental: {
    inspector: {
     holdMode: true
-   }
   }
  }
 };
